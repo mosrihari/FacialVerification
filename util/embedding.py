3909fac92d3640574febe10e251dd875d8846777
Srihari MOHAN <srihari_mohan@nec.com.sg>
2021-04-25 10:06:11 +0800

    less sized model

--- util/embedding.py
+++ util/embedding.py
@@ -2,7 +2,6 @@ from util.faceextract import extract_face
 import numpy as np
 from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
-from keras.models import load_model
 
 def get_embeddings(filenames):
     # extract faces
@@ -12,8 +11,7 @@ def get_embeddings(filenames):
     # prepare the face for the model, e.g. center pixels
     samples = preprocess_input(samples, version=2)
     # create a vggface model or Load the model
-    model = load_model("similarity_model")
-    #model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+    model = VGGFace(model='vgg16', include_top=False, input_shape=(224, 224, 3), pooling='avg')
     # perform prediction
     yhat = model.predict(samples)
     return yhat
